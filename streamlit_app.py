205b8a957c72b32c672bbdad5ab1e02b5a712f48
jbarnthouse <jbarnthouse@gmail.com>
2025-11-07 15:02:00 -0700

    Update streamlit_app.py
    add df for smoothie nutrition info

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,6 +47,8 @@ if ingredients_list:
         #st.success('Your Smoothie is ordered!', icon="✅")
         st.success(f"Your smoothie is ordered, {name_on_order}!" , icon="✅")
 
+# new section to display smoothiefroot nutrition information
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
